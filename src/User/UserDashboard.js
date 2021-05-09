8cff0305a99e9813b385060a7bf382596f56479f
Eaint <eaint.rgo@gmail.com>
2021-05-09 21:36:15 +0630

    MayK::Added User Dashboard with order history

--- src/User/UserDashboard.js
+++ src/User/UserDashboard.js
@@ -2,9 +2,11 @@ import React,{useState,useEffect} from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import Layout from '../core/Layout'
 import {Link} from 'react-router-dom'
-import {Card,ListGroup} from 'react-bootstrap'
+import {Card,ListGroup,Table} from 'react-bootstrap'
 import {isAuthenticated} from '../auth/index'
 import {getUser} from '../store/actions/user'
+import moment from 'moment'
+import styles from './UserDashboard.module.scss'
 
 const UserDashboard = () => {
 	const {name,email,role} = isAuthenticated();
@@ -16,14 +18,11 @@ const UserDashboard = () => {
 	}, [])
 
 	const UserInformation = () => (
-		<Card className="mb-4">
-		<Card.Header>
-			<div className="d-flex flex-row justify-content-between align-items-center">
+		<div className={`mb-4 p-4 rounded ${styles.leftPanel}`}>
+			<div className={`d-flex flex-row justify-content-between align-items-center ${styles.headingUnderline}`}>
 				<h5>User Profile</h5>
 				<Link to="/profile/update">Edit</Link>
 			</div>
-		</Card.Header>
-		<Card.Body>
 			<div className="">
 				<p className="text-muted mt-3 mb-2">Name</p>
 				<h6>{user.name}</h6>
@@ -32,67 +31,72 @@ const UserDashboard = () => {
 				<p className="text-muted mt-3 mb-2">Role</p>
 				<h6>{user.role === 0 ? 'Admin' : 'Registered User'}</h6>
 			</div>
-		</Card.Body>
-	</Card>
+		</div>
 	)
-	const UserLinks = () => (
-		<ListGroup>
-			<ListGroup.Item>
+	const UserLinks = () => (	
+		<div className={`mb-4 p-4 rounded ${styles.leftPanel}`}>
+			<div className="">
 				<Link to="/cart">View My Cart</Link>
-			</ListGroup.Item>
-		</ListGroup>
+			</div>
+		</div>	
 	)
 	const BillingInformation = () => (
-		<Card className="mb-4">
-			<Card.Header>
-				<div className="d-flex flex-row justify-content-between align-items-center">
-					<h5>Billing Information</h5>
-					<Link to="/profile/billing">Edit</Link>
-				</div>
-			</Card.Header>
-			<Card.Body>
-				<div className="">
-					<p className="text-muted mt-3 mb-2">Address: </p>
-					<h6>{user.address}</h6>
-					<p className="text-muted mt-3 mb-2">Phone Number: </p>
-					<h6>{user.phone}</h6>
-					<p className="text-muted mt-3 mb-2">Zip Code: </p>
-					<h6>{user.zipcode}</h6>
-				</div>
-			</Card.Body>
-		</Card>
+		<div className={`mb-4 p-4 rounded ${styles.leftPanel}`}>
+			<div className={`d-flex flex-row justify-content-between align-items-center ${styles.headingUnderline}`}>
+				<h5>Billing Information</h5>
+				<Link to="/profile/billing">Edit</Link>
+			</div>
+			<p className="text-muted mt-3 mb-2">Address: </p>
+			<h6>{user.address ? user.address : '-'}</h6>
+			<p className="text-muted mt-3 mb-2">Phone Number: </p>
+			<h6>{user.phone ? user.phone : '-'}</h6>
+			<p className="text-muted mt-3 mb-2">Zip Code: </p>
+			<h6>{user.zipcode ? user.zipcode : '-'}</h6>
+		</div>
 	)
 	const PurchaseHistory = () => (
-		<Card className="mb-4">
-			<Card.Header>
-				<div className="d-flex flex-row justify-content-between align-items-center">
-					<h5>Purchase History</h5>
+		<div className="mb-4 p-4 shadow-1">
+				<div className={`d-flex flex-row justify-content-between align-items-center mb-4 ${styles.headingUnderline}`}>
+					<h5>Recent Orders</h5>
 				</div>
-			</Card.Header>
-			<Card.Body>
-				<ListGroup>
+			<Table hover className={styles.orderHistoryTable}>
+				<thead>
+					<tr>
+					<th>Order</th>
+					<th>Placed On</th>
+					<th>Total</th>
+					<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
 					{
 						user.history && user.history.map(order => (
-							<ListGroup.Item className="d-flex flex-row justify-content-between">
-								<Link to="/order/123123">Order #{order._id}</Link>
-								<label className="text-muted text-dark-50">Status: <span>{order.status}</span></label>
-							</ListGroup.Item>
+							<tr>
+								<td>
+									<Link to={`/user/orders/${order._id}`}>
+										{order._id}
+									</Link>
+								</td>
+								<td>{moment(order.createdAt).format("DD/MM/YYYY")}</td>
+								<td>{order.total}</td>
+								<td>{order.status}</td>
+							</tr>
 						))
 					}
-				</ListGroup>
-			</Card.Body>
-		</Card>
+				</tbody>
+			</Table>
+		</div>
 	)
 	return(
 		<div>
 			<Layout className="container py-5 my-5">
 				<div className="row mt-5">
-					<div className="col-12 col-md-4">
+					<div className='col-12 col-md-4'>
 						{UserInformation()}
+						{BillingInformation()}
 						{UserLinks()}
 					</div>
 					<div className="col-12 col-md-8">
-						{BillingInformation()}
 						{PurchaseHistory()}
 					</div>
 				</div>
